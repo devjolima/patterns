a7e4d5d952b8b6b7acd0d580660b6f9bc04d275c
jonathas lima <jonathas.o.lima@gmail.com>
2018-11-21 23:41:53 -0300

    JWT customizado e swagger

--- Chile/src/main/java/br/com/jl/api/security/SecurityConfig.java
+++ Chile/src/main/java/br/com/jl/api/security/SecurityConfig.java
@@ -1,14 +1,7 @@
 package br.com.jl.api.security;
 
-import java.security.AuthProvider;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,28 +13,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	 
-	 @Autowired
-	 JwtTokenProvider jwtTokenProvider;
+	@Autowired
+	JwtTokenProvider jwtTokenProvider;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-        http
-        .httpBasic().disable()
-        .csrf().disable()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-            .authorizeRequests()
-            .antMatchers("/auth/signin").permitAll()
-            .anyRequest().authenticated()
-        .and()
-        .apply(new JwtConfigurer(jwtTokenProvider));
-		
+
+		http.httpBasic().disable().csrf().disable().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/auth/signin").permitAll()
+				.antMatchers("/configuration/ui").permitAll()
+				.antMatchers("/webjars/**").permitAll()
+				.antMatchers("/swagger-ui.html").permitAll()
+				.antMatchers("/swagger-resources/**").permitAll()
+				.antMatchers("/configuration/security").permitAll()
+				.antMatchers("/v2/api-docs").permitAll()
+				.anyRequest().authenticated().and()
+				.apply(new JwtConfigurer(jwtTokenProvider));
+
 	}
-
-
-	
-
 
 }
