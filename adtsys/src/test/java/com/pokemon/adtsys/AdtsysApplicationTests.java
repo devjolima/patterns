a77a5c8e1608799197d2dc7e808c5cd7efb7de8a
jonathas lima <jonathas.o.lima@gmail.com>
2019-05-31 21:14:39 -0300

    correções e loader

--- adtsys/src/test/java/com/pokemon/adtsys/AdtsysApplicationTests.java
+++ adtsys/src/test/java/com/pokemon/adtsys/AdtsysApplicationTests.java
@@ -2,15 +2,27 @@ package com.pokemon.adtsys;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AdtsysApplicationTests {
+	
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	public void contextLoads() {
+	public void givenAllContracts() throws Exception {
+		
+		mvc.perform(MockMvcRequestBuilders.get("/api/search/find/{cidade}", "dubai"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 }
